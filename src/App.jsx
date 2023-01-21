6a8931a3874650e77709c69e9951048cef737555
Xavier Julies <78542365+XavierJ96@users.noreply.github.com>
2023-01-21 15:42:33 +0200

    Forecast component updated with hourly

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Search from "./Components/Search/Search"
 import CurrentWather from "./Components/Current-Weather/Current-weather"
-import { WEATHER_API_URL, API_KEY } from "./api";
+import { WEATHER_API_URL, API_KEY, Hourly_KEY } from "./api";
 import './App.css'
 import Forecast from "./Components/Forecast/Forecast";
 
@@ -9,37 +9,38 @@ export default function App() {
 
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecastWeather, setForecastWeather] = useState(null);
+  const [forecastHour, setHourForecast] = useState(null);
 
+  
   // Get data using the Lon and Lat received by City to display
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
-
+    // Declare API'S
     const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
     const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
-
+    const hourlyForecast = fetch(`https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${lat}&lon=${lon}&appid=${Hourly_KEY}&units=metric`)
     // returns a single promise
-    Promise.all([currentWeatherFetch, forecastFetch])
+    Promise.all([currentWeatherFetch, forecastFetch, hourlyForecast])
       .then(async (res) => {
         const weatherResponse = await res[0].json();
         const forecastResponse = await res[1].json();
+        const hourlyResponse = await res[2].json();
         // store data received
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
         setForecastWeather({ city: searchData.label, ...forecastResponse });
+        setHourForecast({ city: searchData.label, ...hourlyResponse })
       })
 
       // If the above fails
       .catch((error) => console.log(error))
-
   }
-
-  console.log(forecastWeather)
 
   return (
     <div className="overflow-y-scroll w-full h-screen py-20">
       <div className="w-full max-w-[550px] mx-auto px-2 py-2 space-y-12" >
         <Search onSearchChange={handleOnSearchChange} />
         {currentWeather && <CurrentWather data={currentWeather} />}
-        {forecastWeather && <Forecast data={forecastWeather} />}
+        {forecastWeather && <Forecast data={forecastWeather} hourData={forecastHour} />}
       </div>
     </div>
   );
--- src/Components/Forecast/Forecast.jsx
+++ src/Components/Forecast/Forecast.jsx
@@ -1,34 +1,31 @@
 import { Accordion, AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel } from "react-accessible-accordion"
 import { TbTemperatureCelsius } from "react-icons/tb";
 
-const WEEK_DAYS = [
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat",
-    "Sun"
-];
 
-function Forecast({ data }) {
-    // Reorders it so that the day specified by the variable dayInWeek becomes the first element of the array
-    const dayInWeek = new Date().getDay();
-    const forecastDays = WEEK_DAYS.slice(dayInWeek, WEEK_DAYS.length)
-        .concat(WEEK_DAYS.slice(0, dayInWeek))
+function Forecast({hourData }) {
 
-    return (
-        <div className="w-full font-[rubik] py-4 px-8 text-white bg-[#162635]/80 space-y-4">
-            <label className="text-2xl">Daily</label> 
+       // Filter the data to only get the items starting from the current time
+       const currentTime = new Date();
+       const nextItems = hourData.list.filter(item => {
+         const itemTime = new Date(item.dt_txt);
+         return itemTime >= currentTime;
+       });
+
+       return (
+        <div className="w-full font-[rubik] py-4 px-8 text-white bg-[#162635]/80 space-y-4 rounded-xl">
+            <label className="text-2xl">Hourly forecast</label> 
             {/* Iterate through the data to get 5 items */}
              <section className="flex justify-evenly">
-                {data.list.splice(0, 5).map((item, index) => (
-                    <div key={index} className="flex flex-col items-center">
-                        <label htmlFor="">{forecastDays[index]}</label>
-                        <img alt="weather" src={`icons/${item.weather[0].icon}.png`} className="w-10"/>
-                        <label className="flex items-center">{Math.round(item.main.temp)}<TbTemperatureCelsius /></label>
-                    </div>
-                ))}
+                {nextItems.slice(0, 5).map((item, index) => {
+                    const time = new Date(item.dt_txt);
+                    return (
+                      <div key={index} className="flex flex-col items-center">
+                          <label htmlFor="">{time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</label>
+                          <img alt="weather" src={`icons/${item.weather[0].icon}.png`} className="w-10"/>
+                          <label className="flex items-center">{Math.round(item.main.temp)}<TbTemperatureCelsius /></label>
+                      </div>
+                    )
+                })}
             </section>
         </div>
     )
